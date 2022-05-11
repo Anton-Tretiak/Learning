445c55ef32e968b5ae3ff1680fe93056558002e5
Anton-Tretiak <105153156+Anton-Tretiak@users.noreply.github.com>
2022-05-11 13:03:26 +0300

    Mini calc in object

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-// метод this
-let obj = {
-    age_1: 18,
-    age_2: 19,
-    sumOfAges: function () {
-        return this.age_1 + this.age_2;
+let calculator = {
+    read: function () {
+        this.num1 = +prompt('Enter the first num','');
+        this.num2 = +prompt('Enter the second num','');
+    },
+    sum: function () {
+        return (this.num1 + this.num2);
+    },
+    mul: function () {
+        return (this.num1 * this.num2);
     }
 }
-console.log(obj.sumOfAges());
+
+
+
+calculator.read();
+console.log( calculator.sum() );
+console.log( calculator.mul() );
+console.log(calculator);
