dccdf80a90a4e3d4083f6cfb9978665e426dbcc2
Anton-Tretiak <105153156+Anton-Tretiak@users.noreply.github.com>
2022-05-15 01:45:54 +0300

    Двумерный массив

--- script.js
+++ script.js
@@ -1,35 +1,10 @@
-let user = [
-    {
-      name: "John",
-      age: 30
-    },
-    {
-      name: "Bob",
-      age: 21
-    },
-    {
-      name: "Anna",
-      age: 19
-    }
+let arr = [
+    [1, 2, 3],
+    [4, 5, 6],
 ];
 
-// Получаем обьект из массива обьектов по заданному параметру
-
-for (let i = 0; i < user.length; i++) {
-    if (user[i].name == 'Bob') {
-        object = user[i];
-        break;
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+        console.log(arr[i][j]);
     }
 }
-console.log(object);
-console.log(user);
-
-// Удаляем обьект из массива обьектов если имя = Анна
-
-for (let i = 0; i < user.length; i++) {
-    if (user[i].name == 'Anna') {
-        delete user[i];
-    }
-}
-console.log(user);
-
