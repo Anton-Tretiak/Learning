65a75ab1ac5dda745f8814e00ca29a99faafbfda
Anton-Tretiak <105153156+Anton-Tretiak@users.noreply.github.com>
2022-05-18 13:29:34 +0300

    Разделение массива на подмассивы по n элементов

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let arr = [
-    [1, 2, 3],
-    [4, 5, 6],
-];
-
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-        console.log(arr[i][j]);
-    }
+let nums = [1,2,3,4,5,6,7,8,9,10]
+let size = 3; 
+let subarray = []; 
+for (let i = 0; i <Math.ceil(nums.length/size); i++){
+    subarray[i] = nums.slice((i*size), (i*size) + size);
 }
+console.log(subarray);
