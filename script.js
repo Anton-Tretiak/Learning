e3139cf1084cd53c5771db2d7a61fd49f990577a
Anton-Tretiak <105153156+Anton-Tretiak@users.noreply.github.com>
2022-05-19 02:11:35 +0300

    35. leetcode task

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let list1 = [1,2,4], list2 = [1,3,4];
-let arr = [].concat(list1, list2);
-arr.sort(function (a, b) {
-    return a - b;
-});
+let nums = [1,3,6];
+let target = 2;
 
-console.log(arr);
+if (nums.includes(target)) {
+    console.log(nums.indexOf(target));
+}
+else {
+    nums.push(target); // закидываем число в массив
+    nums.sort(function (a,b) {  // сортируем массив
+        return a - b;
+    })
+    console.log(nums.indexOf(target)); // выводим индекс таргета
+}
+console.log(nums);
