758a0252e2d86686c7873885dc50a2f08f443d2c
Anton-Tretiak <105153156+Anton-Tretiak@users.noreply.github.com>
2022-05-31 15:21:45 +0300

    Сортировка только чётных чисел в массиве

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let str = 'lana banana';
+let arr = [3,66,5,7,2,8,99,57,64]
+let obb = []
+let len = arr.length
+let counter = 0
+console.log(arr);
 
-function capitalize(str) {
-    let arr = str.split('');
-    arr[0] = arr[0].toUpperCase();
-    return arr.join('');
+for (let i = 0; i < len; i++) { // Создаем массив чётных чисел
+    if (arr[i] % 2 == 0) {
+        obb.push(arr[i])
+    }
+}
+obb.sort((a,b) => { return a - b }) // Сортируем массив чётных чисел
+
+for (let i = 0; i < len; i++) { // Проверяем если число чётное, то заменить его на эл из массива чётных
+    if (arr[i] % 2 == 0) {
+        arr[i] = obb[counter]
+        counter += 1
+    }
 }
 
-console.log(capitalize(str));
+console.log(arr) // начальный массив
+console.log(obb) // конечный массив
